4abf567d43a4d7102f6dcf41fc426434b3ca4638
s1tes-maker <s1tes-maker@ya.ru>
2022-06-22 20:07:10 +0300

    menu-role seeder2

--- database/seeders/MenuRoleSeeder.php
+++ database/seeders/MenuRoleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class MenuRolesSeeder extends Seeder
+class MenuRoleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
