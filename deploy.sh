7e759ada75597a35f067e5e3f793eb677a56eaa5
s1tes-maker <s1tes-maker@ya.ru>
2022-07-06 23:50:52 +0300

    deploy sh bash

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 readonly path="../var/www/domains/ovz1.wallc.gmzem.vps.myjino.ru/"
 pwd
-cp -r ../../cgi-bin/forcopy/ .
+cp -r ../../cgi-bin/forcopy/ ./
 
 php artisan migrate
 php artisan db:seed
