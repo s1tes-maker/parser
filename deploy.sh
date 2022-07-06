48051575f23086a942fea0f9a1f9d449cba41373
s1tes-maker <s1tes-maker@ya.ru>
2022-07-06 21:50:33 +0300

    deploy sh bash

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+readonly path="../var/www/domains/ovz1.wallc.gmzem.vps.myjino.ru/"
+cp -r forcopy/ $path
+cd $path
+php artisan migrate
+php artisan db:seed
+php artisan key:generate
+php artisan storage:link
+php artisan cache:clear
+php artisan config:clear
+php artisan config:cache
+chmod -R 777 storage/logs
+chmod -R 777 storage/framework
+
+
+
